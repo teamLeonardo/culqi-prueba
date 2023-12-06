775e051931fcd4bd8036dea3b6882264797aec4c
teamLeonardo <leonardosm3.14@gmail.com>
2023-12-06 05:47:52 -0500

    feat pwa header 2

--- src/modules/recharge_payments/components/Header.ui.vue
+++ src/modules/recharge_payments/components/Header.ui.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-[10%] max-h-[65px] bg-black text-white">
+    <div class="w-full h-[10%] lg:max-h-[65px] bg-black text-white">
         header
     </div>
 </template>
