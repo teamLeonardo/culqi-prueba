f7e3a9ea0af01be44119189ac1324116a9a03407
teamLeonardo <leonardosm3.14@gmail.com>
2023-12-11 17:40:48 -0500

    change rollback axios conf

--- src/shared/axios.conf.ts
+++ src/shared/axios.conf.ts
@@ -4,7 +4,6 @@ const CulqiAxios = axios.create({
     baseURL: import.meta.env.VITE_BASE_API, 
     headers : {
         'Authorization': `Bearer ${import.meta.env.VITE_TOKEN}`,
-        "Referrer-Policy": "unsafe_url" 
     }
 });
 
