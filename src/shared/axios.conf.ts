c654a84518bdb2762ac783a4d8f2ea5d51c51ff2
teamLeonardo <leonardosm3.14@gmail.com>
2023-12-11 14:06:12 -0500

    fix server

--- src/shared/axios.conf.ts
+++ src/shared/axios.conf.ts
@@ -4,6 +4,7 @@ const CulqiAxios = axios.create({
     baseURL: import.meta.env.VITE_BASE_API, 
     headers : {
         'Authorization': `Bearer ${import.meta.env.VITE_TOKEN}`,
+        "Referrer-Policy": "unsafe_url" 
     }
 });
 
