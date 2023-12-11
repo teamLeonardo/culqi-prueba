c534860f62bbf449b6b79b459448c26e977a364c
teamLeonardo <leonardosm3.14@gmail.com>
2023-12-11 13:35:53 -0500

    fix cors

--- src/shared/axios.conf.ts
+++ src/shared/axios.conf.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const CulqiAxios = axios.create({
-    baseURL: "https://corsproxy.io/?" + import.meta.env.VITE_BASE_API, 
+    baseURL: import.meta.env.VITE_BASE_API, 
     headers : {
         'Authorization': `Bearer ${import.meta.env.VITE_TOKEN}`,
     }
